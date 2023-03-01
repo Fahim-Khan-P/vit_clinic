8b4809052614f44b27eb507f0380f26b66880b72
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-01 12:18:26 +0430

    remove table

--- data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(55) NOT NULL,
-    date_of_birth DATE,
-    escape_attempts INTEGER,
-    neutered BOOLEAN,
-    weight_kg DECIMAL(6, 3)
-);
-
