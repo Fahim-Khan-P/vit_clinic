1ca5d7085a48dc554f3f0cf60d3c21292cdb7f96
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-02 22:37:03 +0430

    Update animals weights and make it COMMIT!

--- queries.sql
+++ queries.sql
@@ -31,3 +31,5 @@ DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 SAVEPOINT my_savepoint;
 UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
