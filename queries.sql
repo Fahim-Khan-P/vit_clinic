ee3104c6841f5b3f7e3cdaec54757fa93be1115c
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-02 22:08:08 +0430

    Update species column and make commit!

--- queries.sql
+++ queries.sql
@@ -12,3 +12,8 @@ UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
