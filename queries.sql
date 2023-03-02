4719b1c8108a3323ce78eb4e8585ff3101eb0ce7
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-02 22:38:11 +0430

    Find amount of all animals by builted (count) fun

--- queries.sql
+++ queries.sql
@@ -33,3 +33,5 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO my_savepoint;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+
+SELECT COUNT(*) FROM animals;
