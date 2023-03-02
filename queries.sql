0eaecd83bf747074a679a423981eab7ce111b6d1
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-02 22:09:06 +0430

    Delete all records!

--- queries.sql
+++ queries.sql
@@ -18,3 +18,6 @@ UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
