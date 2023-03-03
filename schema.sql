5cce0cef1fb7afa9b0f3f08c4d92928489bd2081
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-03 22:05:32 +0430

    Create vets and specialization tables!

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(55) NOT NULL,
+    name VARCHAR(555) NOT NULL,
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
@@ -25,3 +25,36 @@ ALTER TABLE animals ADD COLUMN species_id INTEGER;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INTEGER;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specialization (
+  id SERIAL PRIMARY KEY,
+  species_id INTEGER,
+  vets_id INTEGER,
+  CONSTRAINT FK_species
+  FOREIGN KEY(species_id)
+  REFERENCES species(id),
+
+  CONSTRAINT FK_vets
+  FOREIGN KEY(vets_id)
+  REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY,
+  vets_id INTEGER,
+  animal_id INTEGER,
+  CONSTRAINT FK_vets
+  FOREIGN KEY(vets_id)
+  REFERENCES vets(id),
+
+  CONSTRAINT FK_animals
+  FOREIGN KEY(animal_id)
+  REFERENCES animals(id),
+)
